d1e7af507c9bf5ec8f61d209fcd8d14335ba2c18
lanyuanxiaoyao <lanyuanxiaoyao@gmail.com>
2020-11-08 17:06:21 +0800

    升级 squirrel core 到 0.3.96-SNAPSHOT

--- src/utils/squirrel.js
+++ src/utils/squirrel.js
@@ -1,14 +1,26 @@
-import SquirrelUtools from './squirrel-utools-0.3.89-SNAPSHOT';
+import store from '../store'
+import SquirrelUtools from './squirrel-utools-0.3.96-SNAPSHOT';
 
 const Squirrel = SquirrelUtools.com.lanyuanxiaoyao.squirrel.utools
+Squirrel.debug('true')
 
-window.squirrel = Squirrel
-
-export default {
+window.squirrel = {
   info: () => JSON.parse(Squirrel.info('default')),
   sites: () => JSON.parse(Squirrel.sites('SEARCH')),
   categories: () => JSON.parse(Squirrel.categories('SEARCH')),
+  downloader: () => JSON.parse(Squirrel.downloader()),
   page: async request => eval(`(${await Squirrel.page(JSON.stringify(request))})`),
+  change: request => JSON.parse(Squirrel.change(request)),
+  save: information => Squirrel.save(JSON.stringify(information)),
   fetch: () => JSON.parse(Squirrel.fetch()),
-  save: information => Squirrel.save(JSON.stringify(information))
+  load: information => JSON.parse(Squirrel.load(information)),
+  imports: sites => {
+    JSON.parse(Squirrel.imports(sites))
+    store.commit('updateSitesAndCategories')
+  },
+  debug: enable => JSON.parse(Squirrel.debug(enable))
+}
+
+export default {
+  ...window.squirrel
 }
