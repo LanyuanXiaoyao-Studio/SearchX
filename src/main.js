d3a524d3d768a91d3eda5685ea27c0b73973a370
lanyuanxiaoyao <lanyuanxiaoyao@gmail.com>
2021-01-15 13:17:25 +0800

    整体初始化时加入获取后端数据的进度显示

--- src/main.js
+++ src/main.js
@@ -2,14 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
-Vue.config.productionTip = false
-
 import './plugins/ant'
 import './plugins/masonry'
 
+Vue.config.productionTip = false
+
 if (window.squirrelInitialReady) {
+  let key = 'updatable'
+  Vue.prototype.$message.loading({content: '加载数据...', key})
   window.squirrelInitialReady()
+        .then(result => Vue.prototype.$message.success({content: '加载完成', key}))
+        .catch(error => Vue.prototype.$message.error({content: error, key}))
 }
 
 new Vue({
