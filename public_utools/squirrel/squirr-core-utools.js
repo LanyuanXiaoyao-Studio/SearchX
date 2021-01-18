ded75fb4e70c6b11e93528fa3655a8fb16f5bd1f
lanyuanxiaoyao <lanyuanxiaoyao@gmail.com>
2021-01-18 17:36:19 +0800

    升级 squirrel-core 版本

--- public_utools/squirrel/squirr-core-utools.js
+++ public_utools/squirrel/squirr-core-utools.js
@@ -90,5 +90,5 @@ window.put = data => {
   }
 }
 
-const squirrelLib = require('./squirrel-utools-0.3.120-SNAPSHOT')
+const squirrelLib = require('./squirrel-utools-0.3.129-SNAPSHOT')
 window.squirrelLib = squirrelLib
