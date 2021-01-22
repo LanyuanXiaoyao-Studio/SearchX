899d7ac9be57304669a1f727f9eec0b9edf1ca47
lanyuanxiaoyao <lanyuanxiaoyao@gmail.com>
2021-01-22 16:08:16 +0800

    调整 uTools 构建脚本, 版本号从 package.json 获取

--- scripts/pre-build-utools.js
+++ scripts/pre-build-utools.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const path = require('path')
 const {batchCopyFilesSync, batchDeleteFilesSync, listDirExcludeFiles} = require('./utils')
 
@@ -6,11 +7,41 @@ batchDeleteFilesSync(listDirExcludeFiles(path.join(__dirname, '..', 'public'), [
 let publicPath = path.join(__dirname, '..', 'public')
 let publicSourcePath = path.join(__dirname, '..', 'public_utools')
 
-batchCopyFilesSync([
-    [path.join(publicSourcePath, 'preload.js'), `${publicPath}/preload.js`],
-    [path.join(publicSourcePath, 'plugin.json'), `${publicPath}/plugin.json`],
-    [path.join(publicSourcePath, 'squirrel'), `${publicPath}/squirrel`],
-    [path.join(publicSourcePath, 'node_modules'), `${publicPath}/node_modules`],
+let packageFile = fs.readFileSync(path.join(__dirname, '..', 'package.json'), {encoding: 'utf-8'})
+let version = JSON.parse(packageFile)['version']
+let pluginFile = JSON.stringify({
+  'pluginName': 'SearchX',
+  'author': 'lanyuanxiaoyao',
+  'homepage': 'https://github.com/LanyuanXiaoyao-Studio/SearchX',
+  'description': '海纳百川, 搜遍天下',
+  'version': version,
+  'logo': 'logo.png',
+  'main': 'index.html',
+  'preload': 'preload.js',
+  'pluginSetting': {
+    'single': true,
+    'height': 580
+  },
+  'features': [
+    {
+      'code': 'torrent',
+      'explain': '资源搜索',
+      'cmds': [
+        'Torrent',
+        'BT',
+        '资源搜索',
+        '种子搜索',
+        'SearchX'
+      ]
+    }
+  ]
+})
+fs.writeFileSync(`${publicPath}/plugin.json`, pluginFile)
 
-    [path.join(__dirname, '..', 'icons', 'icon.png'), `${publicPath}/logo.png`],
+batchCopyFilesSync([
+  [path.join(publicSourcePath, 'preload.js'), `${publicPath}/preload.js`],
+  [path.join(publicSourcePath, 'squirrel'), `${publicPath}/squirrel`],
+  [path.join(publicSourcePath, 'node_modules'), `${publicPath}/node_modules`],
+
+  [path.join(__dirname, '..', 'icons', 'icon.png'), `${publicPath}/logo.png`],
 ])
