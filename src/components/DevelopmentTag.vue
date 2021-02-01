d1303408816f8c14512d398d65f6a6fc0dc708ac
lanyuanxiaoyao <lanyuanxiaoyao@gmail.com>
2021-02-01 11:24:07 +0800

    提取一个"开发中"标签

---
+++ src/components/DevelopmentTag.vue
@@ -0,0 +1,18 @@
+<template>
+  <span class="development-tag">
+    开发中
+  </span>
+</template>
+
+<style
+    lang="stylus"
+    scoped
+>
+.development-tag
+  background-color: #ff000095
+  border-radius: 10px
+  color: white;
+  padding: 1px 6px 1px 6px
+  margin: 0 0 0 2px
+  font-size: 0.6rem
+</style>
